205ff224874c04eb6d916956e7b35b7e57568b63
eplmason <e.mason@newcastle.ac.uk>
2016-02-02 15:19:23 +0000

    commit 3 - fixed TCS shader

--- displaceTCS.glsl
+++ displaceTCS.glsl
@@ -11,10 +11,12 @@ out Vertex {
 
 void main () {
 	gl_TessLevelInner [0] = 4;
+	gl_TessLevelInner [1] = 4;
+	gl_TessLevelOuter [0] = 8;
 	gl_TessLevelOuter [1] = 8;
 	gl_TessLevelOuter [2] = 8;
 	gl_TessLevelOuter [3] = 8;
-	gl_TessLevelOuter [4] = 8;
+	
 
 	gl_out [ gl_InvocationID ]. gl_Position = gl_in [ gl_InvocationID ]. gl_Position;
 }
